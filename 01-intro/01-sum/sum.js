3d965aa511d8c0eeec73bb0ec3581b0358237f83
elenamedvedeva <leeloo3m@yandex.ru>
2022-04-21 14:43:57 +0300

    intro

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,13 @@
 function sum(a, b) {
-  /* ваш код */
+
+  if (typeof a == "number" && typeof b == "number") {
+    return a + b;
+  } else {
+    
+    throw new TypeError("аргументы не являются числами");
+  }
+  
+  
 }
 
 module.exports = sum;
