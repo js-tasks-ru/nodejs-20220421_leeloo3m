e35476b6575a75f60d925db8c85005c63d1da63c
elenamedvedeva <leeloo3m@yandex.ru>
2022-05-21 20:09:50 +0300

    '6.2'

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,43 @@
+const Product = require('../models/Product');
+const mappersProduct = require('../mappers/product');
+const mongoose = require('mongoose');
+
+//запрос делается по ObjectID.
+//ObjectId должен быть валидным, если не валидный то 400
+//если товара с таким ObjectID нет то 404
+
+//1. пишем функцию для получения массива сабкатегорий и массива продуктов
+function list(arr) {
+  return arr ? arr.map(el => mappersProduct(el)) : [];
+}
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const subcategoryList = await Product.find({subcategory});
+  ctx.body = {products: list(subcategoryList)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productsList = await Product.find();
+  ctx.body = {products: list(productsList)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if(!mongoose.isValidObjectId(ctx.params.id)){
+    ctx.status = 400;
+    ctx.body = 'invalid id';
+    return;
+  };
+  const prod = await Product.findById(ctx.params.id);
+  if(!prod){
+    ctx.status = 404;
+    ctx.body = 'product not found';
+  } else {
+    ctx.body = {products: mappersProduct(prod)};
+  }
+  
 };
 
--- 06-mongodb-mongoose/02-rest-api/mappers/product.js
+++ 06-mongodb-mongoose/02-rest-api/mappers/product.js
@@ -1,3 +1,5 @@
+
+
 module.exports = function mapProduct(product) {
   return {
     id: product.id,
